0b4c0381c12a74d0fc0fdfd4a9e09fce91b25079
Jayce Fowler <jaycefowler@outlook.com>
2025-01-09 11:11:37 -0500

    add repeating message

--- Fowler-Jayce-A1-Mission/Program.cs
+++ Fowler-Jayce-A1-Mission/Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Jayce!");
- 
+﻿// writes a message that the user can see and respond to
+Console.WriteLine("type in message!");
+// Tells what message for the string to right
+string message;
+// will wait until the user types something and set "message" to what the user types in the console
+message = Console.ReadLine();
+Console.Write(message);
+Console.Write(" ");
+Console.Write(message);
+Console.Write(" ");
+Console.Write(message);
+Console. WriteLine(" ");
